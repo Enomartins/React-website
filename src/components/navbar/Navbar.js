b66837acb38484e60acf0e78897134a59fc0ba9f
Enomartins <enowebservices@gmail.com>
2021-01-11 08:45:54 +0100

    first commit

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Nav, NavbarContainer } from './Navbar.elements';
+import {   } from '../../globalStyles'
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                Test
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar
