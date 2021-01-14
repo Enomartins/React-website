3f147dd1ac383d3fc9f3fdbcc2b76870244bfccc
Enomartins <enowebservices@gmail.com>
2021-01-14 10:33:47 +0100

    second commit

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,12 @@
+export const homeObjOne = {
+    lightBg: false,
+    primary: true,
+    imgStart: '',
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Get Started',
+    description: 'We help business owners increase their revenue. Our team of unique specialists can help you achieve your business goals',
+    headline: 'Lead Generation Specialista for Online Businesses',
+    lightText: true,
+    topLine: 'Marketing Agency'
+}
