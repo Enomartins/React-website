be81099217658aa1a5969d805960dcdd65b9f6a8
Enomartins <enowebservices@gmail.com>
2021-01-16 10:19:00 +0100

    added the reusable components to Home page

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -1,4 +1,37 @@
 export const homeObjOne = {
+    primary: true,
+    lightBg: false,
+    lightTopLine: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Marketing Agency',
+    headline: 'Lead Generation Specialist for Online Businesses',
+    description:
+        'We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.',
+    buttonLabel: 'Get Started',
+    imgStart: '',
+    img: require('../../images/app.svg'),
+    alt: 'Credit Card',
+    start: ''
+}
+
+export const homeObjTwo = {
+    lightBg: true,
+    primary: true,
+    imgStart: '',
+    lightTopLine: true,
+    lightTextDesc: false,
+    buttonLabel: 'Get Started',
+    description: 'We help business owners increase their revenue. Our team of unique specialists can help you achieve your business goals',
+    headline: 'Lead Generation Specialista for Online Businesses',
+    lightText: false,
+    topLine: 'Marketing Agency',
+    img: require('../../images/me.jpg'),
+    alt: 'Card',
+    start: ''
+}
+
+export const homeObjThree = {
     lightBg: false,
     primary: true,
     imgStart: '',
@@ -8,5 +41,24 @@ export const homeObjOne = {
     description: 'We help business owners increase their revenue. Our team of unique specialists can help you achieve your business goals',
     headline: 'Lead Generation Specialista for Online Businesses',
     lightText: true,
-    topLine: 'Marketing Agency'
+    topLine: 'Marketing Agency',
+    img: require('../../images/dev.svg'),
+    alt: 'Image',
+    start: ''
+}
+
+export const homeObjFour = {
+    lightBg: false,
+    primary: true,
+    imgStart: '',
+    lightTopLine: true,
+    lightTextDesc: true,
+    buttonLabel: 'Get Started',
+    description: 'We help business owners increase their revenue. Our team of unique specialists can help you achieve your business goals',
+    headline: 'Lead Generation Specialista for Online Businesses',
+    lightText: true,
+    topLine: 'Marketing Agency',
+    img: require('../../images/react.svg'),
+    alt: 'Credit',
+    start: ''
 }
--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { InfoSection } from '../../components'
-import { homeObjOne } from './Data'
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data'
 
 const Home = () => {
     return (
         <>
-            <InfoSection {...homeObjOne} /> 
-        </ >
+            <InfoSection {...homeObjOne} />
+            <InfoSection {...homeObjTwo} />
+            <InfoSection {...homeObjThree} />
+            <InfoSection {...homeObjFour} />
+        </>
     )
 }
 
